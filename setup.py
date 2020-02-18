c3c3a120b8ee9d588b8f1394b7c1aa231e6c3997
Baghi <qbaghi@gs66-azathoth.ndc.nasa.gov>
2020-02-18 11:45:22 -0500

    Updated setup.py again

--- setup.py
+++ setup.py
@@ -1,68 +1,68 @@
-# Always prefer setuptools over distutils
+import ez_setup
+ez_setup.use_setuptools()
 from setuptools import setup, find_packages
-from os import path
-# io.open is needed for projects that support Python 2.7
-# It ensures open() defaults to text mode with universal newlines,
-# and accepts an argument to specify the text encoding
-# Python 3 only projects can skip this import
-from io import open
-
-here = path.abspath(path.dirname(__file__))
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
+setup(name='microspy',
+      version='1.0.0',
+      author='Quentin Baghi',
+      description='',
+      long_description=open('README.md').read(),
+      license='see LICENSE.txt',
+      keywords="",
+      packages=find_packages(exclude='docs'),
+      install_requires=['numpy', 'scipy', 'pyfftw'])
 
-setup(
-    name='microspy',
-    version='1.0.0',
-    description='Analyse data from the MICROSCOPE mission science center',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/qbaghi/microspy',
-    author='Quentin Baghi',
-    author_email='quentin.baghi@protonmail.com',
-    classifiers=[  # Optional
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish
-        'License :: OSI Approved :: GNU License',
-
-        # Python versions
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-
-    keywords='data processing',
-    packages=find_packages(),
-    python_requires='>=3.5', install_requires=['pyfftw', 'scipy', 'numpy']
-
-)
-
-# import ez_setup
+# # Always prefer setuptools over distutils
 # from setuptools import setup, find_packages
-# ez_setup.use_setuptools()
+# from os import path
+# # io.open is needed for projects that support Python 2.7
+# # It ensures open() defaults to text mode with universal newlines,
+# # and accepts an argument to specify the text encoding
+# # Python 3 only projects can skip this import
+# from io import open
+#
+# here = path.abspath(path.dirname(__file__))
+# # Get the long description from the README file
+# with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+#     long_description = f.read()
 #
 #
-# setup(name='microspy',
-#       version='1.0.0',
-#       author='Quentin Baghi',
-#       description='',
-#       long_description=open('README.md').read(),
-#       license='see LICENSE.txt',
-#       keywords="",
-#       packages=find_packages(exclude='docs'),
-#       install_requires=['numpy', 'scipy', 'pyfftw'])
+# # Arguments marked as "Required" below must be included for upload to PyPI.
+# # Fields marked as "Optional" may be commented out.
+#
+# setup(
+#     name='microspy',
+#     version='1.0.0',
+#     description='Analyse data from the MICROSCOPE mission science center',
+#     long_description=long_description,
+#     long_description_content_type='text/markdown',
+#     url='https://github.com/qbaghi/microspy',
+#     author='Quentin Baghi',
+#     author_email='quentin.baghi@protonmail.com',
+#     classifiers=[  # Optional
+#         #   3 - Alpha
+#         #   4 - Beta
+#         #   5 - Production/Stable
+#         'Development Status :: 3 - Alpha',
+#
+#         # Indicate who your project is intended for
+#         'Intended Audience :: Developers',
+#         'Topic :: Software Development :: Build Tools',
+#
+#         # Pick your license as you wish
+#         'License :: OSI Approved :: GNU License',
+#
+#         # Python versions
+#         'Programming Language :: Python :: 3',
+#         'Programming Language :: Python :: 3.4',
+#         'Programming Language :: Python :: 3.5',
+#         'Programming Language :: Python :: 3.6',
+#         'Programming Language :: Python :: 3.7',
+#     ],
+#
+#     keywords='data processing',
+#     packages=find_packages(),
+#     python_requires='>=3.5', install_requires=['pyfftw', 'scipy', 'numpy']
+#
+# )
